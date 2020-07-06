75a172191d4c98050c808d801c86859d783c0961
acpadarshpandey <adarsh.1999p@gmail.com>
2020-07-06 14:34:49 +0530

    uichange

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
 import Calculator from './component/index';
-import back from './1.jpg'
 function App() {
   return (
     <div className="App">
-         <h2> Your Calculator </h2>
+         <h2> Your Calculator!! </h2>
          <Calculator/>
     </div>
   );
--- src/component/index.js
+++ src/component/index.js
@@ -18,9 +18,12 @@ class Calculator extends React.Component {
             })
         }
         catch (Error) {
+            alert(Error);
             this.setState({
                 result: "Error"
             })
+
+
         }
     }
     clear = () => {
@@ -37,33 +40,34 @@ class Calculator extends React.Component {
 
     render() {
         return (
-            <div>
-                <form>
-                    <input className="input"
+            <div id="main">
+                <form >
+                    <input placeholder="0" className="input"
                         type="text" value={this.state.result}></input>  </form>
                 <div style={{ justifyContent: "center" }}>
                     <div className="grid-container">
-                        <div className="grid-item"> <button className="btn" onClick={this.clear} >Clear</button> </div>
-                        <div className="grid-item">  <button className="btn" onClick={this.backspace} >C</button> </div>
-                        <div className="grid-item"> <button className="btn" name="+" onClick={this.handleClick}>+</button> </div>
-                        <div className="grid-item"> <button className="btn" name="-" onClick={this.handleClick}>-</button> </div>
-                        <div className="grid-item"><button className="btn" name="*" onClick={this.handleClick}>&times;</button> </div>
-                        <div className="grid-item">   <button className="btn" name="/" onClick={this.handleClick}>/</button></div>
-                        <div className="grid-item"><button className="btn" name="." onClick={this.handleClick}>.</button> </div>
-                        <div className="grid-item"><button className="btn" name="(" onClick={this.handleClick}>(</button> </div>
-                        <div className="grid-item"><button className="btn" name=")" onClick={this.handleClick}>)</button> </div>
-                        <div className="grid-item"> <button className="btn" name="7" onClick={this.handleClick}>7</button></div>
-                        <div className="grid-item"> <button className="btn" name="8" onClick={this.handleClick}>8</button></div>
-                        <div className="grid-item">  <button className="btn" name="9" onClick={this.handleClick}>9</button></div>
-                        <div className="grid-item"> <button className="btn" name="4" onClick={this.handleClick}>4</button></div>
-                        <div className="grid-item"> <button className="btn" name="5" onClick={this.handleClick}>5</button> </div>
-                        <div className="grid-item"> <button className="btn" name="6" onClick={this.handleClick}>6</button> </div>
-                        <div className="grid-item"> <button className="btn" name="1" onClick={this.handleClick}>1</button></div>
-                        <div className="grid-item"><button className="btn" name="2" onClick={this.handleClick}>2</button> </div>
-                        <div className="grid-item"> <button className="btn" name="3" onClick={this.handleClick}>3</button> </div>
-                        <div className="grid-item"><button className="btn" name="0" onClick={this.handleClick}>0</button> </div>
-                        <div className="grid-item"> <button className="btn" onClick={this.calculate} id="result" >Result</button></div>
-                        <div className="grid-item"> <button className="btn" onClick={this.clear} >Clear</button> </div>
+                        <button className="btn clear" onClick={this.clear} >Clear</button>
+                        <button className="btn btn1" onClick={this.backspace} >C</button>
+                        <button className="btn btn2" name="+" onClick={this.handleClick}>+</button>
+                        <button className="btn btn2" name="-" onClick={this.handleClick}>-</button>
+                        <button className="btn" name="7" onClick={this.handleClick}>7</button>
+                        <button className="btn" name="8" onClick={this.handleClick}>8</button>
+                        <button className="btn" name="9" onClick={this.handleClick}>9</button>
+                        <button className="btn btn2" name="*" onClick={this.handleClick}>&times;</button>
+                        <button className="btn" name="4" onClick={this.handleClick}>4</button>
+                        <button className="btn" name="5" onClick={this.handleClick}>5</button>
+                        <button className="btn" name="6" onClick={this.handleClick}>6</button>
+                        <button className="btn btn2" name="(" onClick={this.handleClick}>(</button>
+                        <button className="btn" name="1" onClick={this.handleClick}>1</button>
+                        <button className="btn" name="2" onClick={this.handleClick}>2</button>
+                        <button className="btn" name="3" onClick={this.handleClick}>3</button>
+                        <button className="btn btn2" name=")" onClick={this.handleClick}>)</button>
+                        <button className="btn" name="0" onClick={this.handleClick}>0</button>
+                        <button className="btn" name="." onClick={this.handleClick}>.</button>
+                        <button className="btn btn1" onClick={this.calculate} >=</button>
+                        <button className="btn btn2" name="/" onClick={this.handleClick}>/</button>
+
+
 
                     </div>
 
